8cf46c46bc09a9bdb5872e2024eeac0bc662dba6
Yang Lu <yang.youngsoul@gmail.com>
2020-04-22 12:16:04 +0800

    add home page

---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import styles from './Header.module.css';
+import React from 'react';
+import Logo from '../../assets/images/logo.jpg';
+
+const Header = (props) => (
+  <div className={styles.container}>
+    <img src={Logo} alt='Unimelb logo' className={styles.logo}></img>
+    <div className={styles.title}>Rapid Feedback</div>
+    <ul className={'nav justify-content-end ' + styles.mynav}>
+      <li className='nav-item'>
+        <a className='nav-link active' href='#'>
+          Home
+        </a>
+      </li>
+      <li className='nav-item'>
+        <a className='nav-link' href='#'>
+          Administration
+        </a>
+      </li>
+      <li className='nav-item'>
+        <a className='nav-link' href='#'>
+          Assessment
+        </a>
+      </li>
+      <li className='nav-item'>
+        <a className='nav-link' href='#'>
+          Report
+        </a>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Header;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styles from './Home.module.css';
 import { connect } from 'react-redux';
+import Header from '../Header/Header';
 
 class Home extends React.Component {
   render() {
-    if (!this.props.isAuthenticated) {
-      this.props.history.push('/login');
-    }
+    // if (!this.props.isAuthenticated) {
+    //   this.props.history.push('/login');
+    // }
 
     return (
-      <div className={styles.main}>
-        <h1>This is the Home Page</h1>
-      </div>
+      <Fragment>
+        <Header></Header>
+        <div className={styles.main}>
+          <h1>This is the Home Page</h1>
+        </div>
+      </Fragment>
     );
   }
 }
