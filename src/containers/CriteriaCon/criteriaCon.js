f629fd495c1bc2d66d74afce5352588aa9e29b9e
Shun Yin <shun.yin@student.unimelb.edu.au>
2020-04-23 01:37:52 +1000

    Create criteriaCon.js

---
+++ src/containers/CriteriaCon/criteriaCon.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const projectInfoCard = () => {
+  return (
+    <div className="projectcard">
+      <div className="subjectInfo">
+        <h1>COMP90082 Software Project</h1>
+      </div>
+      <div className="extraContent">
+        <div className="projectName">
+          <label>Project Name:</label>
+          <p className="pName">Apollo Program</p>
+        </div>
+        <div className="Assessmemt Target">
+          <label>Assessment Target:</label>
+          <p className="assTarget">Individual</p>
+        </div>
+        <div className="Assessmemt Duration">
+          <label>Assessmemt Duration:</label>
+          <p className="assDur">5 minutes</p>
+        </div>
+        <div className="Created By">
+          <label>Created By:</label>
+          <p className="author">Prof.xxx</p>
+        </div>
+      </div>
+    </div>
+  )
+}
