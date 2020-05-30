4bb6417bec5c30ac4dda14b084ddff0cb6c9d2cb
shun-yin <63029577+shun-yin@users.noreply.github.com>
2020-05-31 03:03:18 +1000

    fix comment modal input bug

--- src/containers/Assess/Marking/CommentsInput.js
+++ src/containers/Assess/Marking/CommentsInput.js
@@ -16,7 +16,7 @@ export default class CommentsInputs extends React.Component {
         return (
             <div>
                 <form onSubmit={(e) => this.submit(e)}>
-                    <input type="text" style={{width:"70%",marginRight:"5%"}} onChange={(e) => this.setState({ input: e.target.value })} value={this.state.input}></input>
+                    <input type="text" style={{width:"70%",marginRight:"5%"}} onChange={(e) => this.setState({ input: e.target.value })} value={this.state.input} required></input>
                     <select id="mySelect" style={{width:"20%"}} onChange={(e) => this.setState({ type: e.target.value })}>
                         <option value="Positive">Positive</option>
                         <option value="Natural">Natural</option>
